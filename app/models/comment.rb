88ab27d5b2b53b83bec57a4f65ac9ada48e23278
John W. M. Carneiro <johnmarques87@gmail.com>
2016-11-05 12:56:43 -0300

    update comentario

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,8 @@ class Comment < ActiveRecord::Base
   # Associations
   belongs_to :commentable, polymorphic: true
   belongs_to :user
+
+  # Scopes
+  default_scope { order('comments.created_at DESC') }
+  scope :created_by, -> (user) { where(user_id: user.id) }
 end
