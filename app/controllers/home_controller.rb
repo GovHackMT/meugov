ffd0fe1a231d3343bfb7fbe8064c649349690172
John W. M. Carneiro <johnmarques87@gmail.com>
2016-11-06 12:47:26 -0300

    limitado em 6 popular

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def index
     @proposals = Proposal.avaliable
-    @popular_proposals = Proposal.avaliable.popular
+    @popular_proposals = Proposal.avaliable.popular.limit(6)
 
     @government_proposals = Proposal.avaliable.government
     @society_proposals = Proposal.avaliable.society
